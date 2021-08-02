4ee488fd0282107cf14028ec0771267b03d797be
prz23 <panruizheng@zju.edu.cn>
2021-08-02 17:15:46 +0800

    2

--- enclave/src/tests.rs
+++ enclave/src/tests.rs
@@ -1,20 +1,15 @@
 use std::prelude::v1::*;
 use std::panic::{catch_unwind, resume_unwind, AssertUnwindSafe};
 
-use num_bigint::{ToBigInt, RandBigInt};
+use num_bigint::{BigInt, Sign, ToBigInt};
 
 pub fn serialize_rand_pk_verify_pad() {
     println!("1");
-    use num_bigint::{ToBigInt, RandBigInt};
 
-    let mut rng = rand::thread_rng();
-    let a = rng.gen_bigint(1000);
+    let a = BigInt::from_bytes_be(Sign::Plus, b"AA");
+    println!("2");
+    let b = BigInt::from_bytes_be(Sign::Plus, b"BB");
 
-    let low = -10000.to_bigint().unwrap();
-    let high = 10000.to_bigint().unwrap();
-    let b = rng.gen_bigint_range(&low, &high);
-
-    // Probably an even larger number.
     println!("{}", a * b);
 }
 
